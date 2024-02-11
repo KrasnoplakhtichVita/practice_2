17e2505dd1697aaec577788ae09d183de6c205d8
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-02-11 15:55:43 +0200

    add task-4

--- src/main.js
+++ src/main.js
@@ -9,10 +9,18 @@ function onHandleSubmit(evt) {
 
   console.log(evt.target.taskName.value.trim());
   createMarkup(evt.target.taskName.value.trim());
+  addTask(evt.target.taskName.value.trim());
   evt.target.reset();
 }
 
 function createMarkup(value) {
   const markup = `<li>${value}</li>`;
   list.insertAdjacentHTML('afterbegin', markup);
+}
+
+function addTask(value) {
+  const array = JSON.parse(localStorage.getItem('ui-theme')) || [];
+
+  array.push(value);
+  localStorage.setItem('ui-theme', JSON.stringify(array));
 }
